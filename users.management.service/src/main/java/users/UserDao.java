bc700c601a5ebd2fd305e2f5c59af0d72712eb99
stavfinz <74550528+stavfinz@users.noreply.github.com>
2021-11-09 12:58:57 +0200

    Commit to upload the good project

--- users.management.service/src/main/java/users/UserDao.java
+++
@@ -1,9 +0,0 @@
-package users;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserDao extends PagingAndSortingRepository<UserEntity, String>{
-	
-	
-
-}
